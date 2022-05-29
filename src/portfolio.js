f0eb998267798f9073830e60b8264cbdec63fdbe
hamzaiqbal2000 <hiqbal.bscs18seecs@seecs.edu.pk>
2022-05-29 13:30:52 +0500

    added links

--- src/portfolio.js
+++ src/portfolio.js
@@ -27,7 +27,7 @@ const projects = [
       'An open-source music player application made using React and Sass that allows use to play song of their choice.',
 
     stack: ['SASS', 'Javascript', 'React'],
-    sourceCode: 'https://github.com',
+    sourceCode: 'https://github.com/hamzaiqbal2000/music-player-react',
     livePreview: 'https://music-player23.herokuapp.com/',
   },
   {
@@ -35,7 +35,7 @@ const projects = [
     description:
       'A platform where teachers and students can connect for online tutions/mentorship. It is created using Sass and react.',
     stack: ['SASS', 'JavaScript', 'React'],
-    sourceCode: 'https://github.com',
+    sourceCode: 'https://github.com/hamzaiqbal2000/learnrr-frontend-react',
     livePreview: 'https://learnrr-classroom.herokuapp.com/',
   },
   {
@@ -43,7 +43,7 @@ const projects = [
     description:
       'An open-source application that allows users to search for the specfic location/information using Ip address. It is created using React, Ipify, and Leaflet ',
     stack: ['Tailwind', 'JavaScript', 'React', 'Ipify', 'Leaflet'],
-    sourceCode: 'https://github.com',
+    sourceCode: 'https://github.com/hamzaiqbal2000/ip-tracker',
     livePreview: 'https://ip-tracker-1.herokuapp.com/',
   },
   {
@@ -51,7 +51,7 @@ const projects = [
     description:
       'An open-source application with a huge database of upcoming and famous games, and users can get information about their favourite games. Created using react, redux.',
     stack: ['SASS', 'JavaScript', 'React', 'Redux'],
-    sourceCode: 'https://github.com',
+    sourceCode: 'https://github.com/hamzaiqbal2000/Ignite-ReactJs',
     livePreview: 'https://ignite23.herokuapp.com/',
   },
   {
